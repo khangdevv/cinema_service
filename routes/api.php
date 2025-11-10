e3b83803f997c9d09b0e9c80917238fa8107d03e
khangdevv <baokhang81104@gmail.com>
2025-11-10 15:52:05 +0700

    update :  comment method uneccessary

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,15 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
+
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/info', [AuthController::class, 'getInfo']);
+
+    // Route::middleware('role:ADMIN')->group(function () {
+    //     Route::get('/admin/accounts', [AccountController::class, 'index']);
+    //     Route::get('/admin/accounts/{id}', [AccountController::class, 'show']);
+    //     Route::put('/admin/accounts/{id}', [AccountController::class, 'update'] );
+    // });
 });
 
 Route::apiResource('accounts', AccountController::class);
